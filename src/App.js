8a9103b40992c1b07ea20e7dd82dee0afaccade0
ezhuun <ljhadf@naver.com>
2018-12-17 14:54:55 +0900

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PhoneForm from './components/PhoneForm';
+import PhoneInfoList from './components/PhoneInfoList';
 
 class App extends Component {
+  id = 2
+  state = {
+    information: [
+      {
+        id:0,
+        name:'이지훈',
+        phone:'010-4033-2290'
+      },
+      {
+        id:1,
+        name:'김초연',
+        phone:'010-7929-7057'
+      }
+    ]
+  }
+  handleCreate = (data) => {
+    const { information } = this.state;
+    this.setState({
+      information: information.concat({id:this.id++, ...data})
+    });
+    console.log(data);
+  }
+  handleRemove = (id) => {
+    const { information } = this.state;
+    this.setState({
+      information: information.filter(info => info.id !== id)
+    })
+  }
+  handleUpdate = (id, data) => {
+    const { information } = this.state;
+    this.setState({
+      information: information.map(
+        info => id === info.id
+          ? { ...info, ...data }
+          : info
+      )
+    })
+  }
   render() {
+    const { information } = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <PhoneForm
+          onCreate={this.handleCreate}
+        />
+        <PhoneInfoList
+          data={information}
+          onRemove={this.handleRemove}
+          onUpdate={this.handleUpdate}
+        />
       </div>
     );
   }
---
+++ src/components/PhoneForm.js
@@ -0,0 +1,45 @@
+//file: src/compenents/PhoneForm.js
+import React, { Component } from 'react';
+
+class PhoneForm extends Component{
+  state = {
+    name:  '',
+    phone: ''
+  }
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.onCreate(this.state);
+    this.setState({
+      name: '',
+      phone: ''
+    });
+  }
+
+  render(){
+    return(
+      <form>
+       <input
+       placeholder="이름"
+       value={this.state.name}
+       onChange={this.handleChange}
+       name="name"
+        />
+        <input
+        placeholder="전화번호"
+        value={this.state.phone}
+        onChange={this.handleChange}
+        name="phone"
+         />
+       <div>{this.state.name} {this.state.phone}</div>
+       <button onClick={this.handleSubmit}>등록</button>
+      </form>
+    );
+  }
+}
+
+export default PhoneForm;
